573905de7a2dd85bef70322d6eaa83159f8675f9
JameelAlhasn <alhsnjmyl18@gmail.com>
2025-07-08 09:20:34 +0300

    Update Stack.h

--- data structures/Stack/Stack.h
+++ data structures/Stack/Stack.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdexcept>
+#include <utility>
+
 template <typename T>
 class Stack
 {
@@ -10,8 +12,10 @@ private:
         Node *prev;
         Node(const T &val) : data(val), prev(nullptr) {}
     };
+
     Node *head;
     int size_;
+
     void clear()
     {
         while (head)
@@ -20,11 +24,57 @@ private:
             head = head->prev;
             delete temp;
         }
+        size_ = 0;
+    }
+
+    void copyFrom(const Stack &other)
+    {
+        if (other.head == nullptr) return;
+        Stack<T> temp;
+        for (Node *cur = other.head; cur != nullptr; cur = cur->prev)
+            temp.push(cur->data);
+        for (Node *cur = temp.head; cur != nullptr; cur = cur->prev)
+            push(cur->data);
     }
 
 public:
     Stack() : head(nullptr), size_(0) {}
     ~Stack() { clear(); }
+
+    Stack(const Stack &other) : head(nullptr), size_(0)
+    {
+        copyFrom(other);
+    }
+
+    Stack &operator=(const Stack &other)
+    {
+        if (this != &other)
+        {
+            clear();
+            copyFrom(other);
+        }
+        return *this;
+    }
+
+    Stack(Stack &&other) noexcept : head(other.head), size_(other.size_)
+    {
+        other.head = nullptr;
+        other.size_ = 0;
+    }
+
+    Stack &operator=(Stack &&other) noexcept
+    {
+        if (this != &other)
+        {
+            clear();
+            head = other.head;
+            size_ = other.size_;
+            other.head = nullptr;
+            other.size_ = 0;
+        }
+        return *this;
+    }
+
     template <typename... Args>
     void emplace(Args &&...args)
     {
@@ -33,49 +83,57 @@ public:
         head = newNode;
         size_++;
     }
-    bool empty()
-    {
-        return !(size_);
-    }
-    void pop()
-    {
-        if (empty())
-            throw std::runtime_error("Stack empty cant pop");
-        Node *temp = head;
-        head = head->prev;
-        delete temp;
-        size_--;
-    }
-    void push(T value)
+
+    void push(const T &value)
     {
         Node *newNode = new Node(value);
         newNode->prev = head;
         head = newNode;
         size_++;
     }
-    int size()
+
+    void pop()
+    {
+        if (empty())
+            throw std::runtime_error("Stack empty can't pop");
+        Node *temp = head;
+        head = head->prev;
+        delete temp;
+        size_--;
+    }
+
+    T &top()
+    {
+        if (empty())
+            throw std::runtime_error("Stack empty can't access top");
+        return head->data;
+    }
+
+    const T &top() const
+    {
+        if (empty())
+            throw std::runtime_error("Stack empty can't access top");
+        return head->data;
+    }
+
+    bool empty() const
+    {
+        return size_ == 0;
+    }
+
+    int size() const
     {
         return size_;
     }
+
     void swap(Stack &other)
     {
         std::swap(head, other.head);
         std::swap(size_, other.size_);
     }
+
     friend void swap(Stack &a, Stack &b)
     {
         a.swap(b);
-    }
-    T &top()
-    {
-        if (empty())
-            throw std::runtime_error("Stack empty cant pop");
-        return head->data;
-    }
-    const T &top() const
-    {
-        if (empty())
-            throw std::runtime_error("Stack empty cant pop");
-        return head->data;
     }
 };
