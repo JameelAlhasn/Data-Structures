55b854637ddc99a56a7c0356423ca249914cc6d1
JameelAlhasn <alhsnjmyl18@gmail.com>
2025-07-22 11:34:04 +0300

    Update UnionFind.h

--- data structures/UnionFind/UnionFind.h
+++ data structures/UnionFind/UnionFind.h
@@ -1,31 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-using namespace std;
-template <typename T>
-struct UnionFind
-{
-    unordered_map<T, T> parent;
-    unordered_map<T, int> rank;
 
-    void make_set(const T &x)
-    {
+template <typename T>
+class UnionFind {
+private:
+    std::unordered_map<T, T> parent;
+    std::unordered_map<T, int> rank;
+
+public:
+    void make_set(const T &x) {
         parent[x] = x;
         rank[x] = 0;
     }
-    T find(const T &x)
-    {
+
+    T find(const T &x) {
         if (parent[x] != x)
             parent[x] = find(parent[x]);
         return parent[x];
     }
 
-    void union_sets(const T &x, const T &y)
-    {
+    void union_sets(const T &x, const T &y) {
         T px = find(x);
         T py = find(y);
-        if (px != py)
-        {
+        if (px != py) {
             if (rank[px] < rank[py])
                 parent[px] = py;
             else if (rank[px] > rank[py])
@@ -37,7 +35,7 @@ struct UnionFind
         }
     }
 
-    bool same_set(const T& x,const T& y){
+    bool same_set(const T& x, const T& y) {
         return find(x) == find(y);
     }
 };
